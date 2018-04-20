bec633e7ed77015237da48f6454c76541e304c50
Randall Harris Haynes <harrishaynes@gmail.com>
2018-04-20 03:51:17 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -11,10 +11,6 @@ class Song < ActiveRecord::Base
     self.artist.name if !!self.artist
   end
   
-  def genre_id=(id)
-    self.genre = Genre.find(id)
-  end
-  
   def genre_name=(name)
     self.genre = Genre.find_or_create_by(:name => name)
   end
